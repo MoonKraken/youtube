2742dcf8b3fcf0ea3e3e929b6236d6d377e7e12e
Ken 🏈 <me163@mac.com>
2022-01-10 13:34:39 -0800

    IntroductionToCDK first iteraiton

---
+++ IntroductionToCDK/test/introduction_to_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IntroductionToCdk from '../lib/introduction_to_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/introduction_to_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IntroductionToCdk.IntroductionToCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
