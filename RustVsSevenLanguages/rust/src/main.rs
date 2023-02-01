b28c61606d079209de04a65b5ce01d6a7de2dbd4
Ken 🏈 <me163@mac.com>
2023-02-01 10:50:08 -0800

    make rust version a bit better

--- RustVsSevenLanguages/rust/src/main.rs
+++ RustVsSevenLanguages/rust/src/main.rs
@@ -1,5 +1,5 @@
-use std::io::stdin;
-fn sum_fn(arr: Vec<&str>) -> i32 {
+use std::{io::stdin, str::SplitWhitespace};
+fn sum_fn(arr: SplitWhitespace) -> i32 {
     arr.into_iter()
         .map(|elem| elem.parse::<i32>().unwrap())
         .sum::<i32>()
@@ -9,7 +9,7 @@ fn main() {
     loop {
         let mut buffer = String::new();
         stdin().read_line(&mut buffer).unwrap();
-        let splits = buffer.split_whitespace().collect();
+        let splits = buffer.split_whitespace();
         let sum = sum_fn(splits);
         println!("{}", sum);
     }
