7fedf43d06ab9cfb73e86ee5bfac26d3e07d0ee2
Victor SANNIER <104424214+vsannier@users.noreply.github.com>
2023-03-08 17:49:27 +0100

    Update Main.hs

--- RustVsSevenLanguages/haskell/app/Main.hs
+++ RustVsSevenLanguages/haskell/app/Main.hs
@@ -1,8 +1,4 @@
 module Main where
 
-sum_fn l = sum $ map (\s->read s :: Integer) l
--- main :: IO ()
-main = do
-  line <- getLine
-  putStrLn $ show $ sum_fn $ words line
-  main
+main :: IO ()
+main = getLine >>= print . sum . map read . words >>= const main
