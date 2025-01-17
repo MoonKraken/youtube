11e4ee108195d6c37830d62be54211be9dfe05ca
Ken 🏈 <me163@mac.com>
2025-01-16 16:12:53 -0800

    HTMX Rust vs Zig

---
+++ HtmxRustZig/htmx_axum/src/main.rs
@@ -0,0 +1,123 @@
+use axum::{
+    Form, Router,
+    extract::State,
+    response::{Html, IntoResponse},
+    routing::{get, put},
+};
+use serde::Deserialize;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::sync::RwLock;
+use tracing::debug;
+
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() {
+    let person = PersonInfo {
+        first_name: "John".to_string(),
+        last_name: "Smith".to_string(),
+        email: "john@companyco.com".to_string(),
+    }; // Run server
+
+    let state = Arc::new(RwLock::new(person));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(
+        listener,
+        Router::new()
+            .route("/", get(index))
+            .route("/contact/1", put(contact_put))
+            .route("/contact/1", get(cancel_edit))
+            .route("/contact/1/edit", get(contact_edit))
+            .with_state(state)
+            .layer(TraceLayer::new_for_http()),
+    )
+    .await
+    .unwrap();
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct PersonInfo {
+    first_name: String,
+    last_name: String,
+    email: String,
+}
+
+fn view_only_person(person: &PersonInfo) -> String {
+    format!(
+        r#"
+        <div hx-target="this" hx-swap="outerHTML">
+            <div><label>First Name</label>: {}</div>
+            <div><label>Last Name</label>: {}</div>
+            <div><label>Email</label>: {}</div>
+            <button hx-get="/contact/1/edit" class="btn primary">
+            Click To Edit
+            </button>
+        </div>
+        "#,
+        person.first_name, person.last_name, person.email,
+    )
+}
+
+async fn index(State(state): State<Arc<RwLock<PersonInfo>>>) -> impl IntoResponse {
+    let person = state.read().await;
+    Html(format!(
+        r#"
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Axum + HTMX</title>
+                <script src="https://unpkg.com/htmx.org"></script>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+            </head>
+            <body>
+            {}
+            </body>
+            </html>
+        "#,
+        view_only_person(&person)
+    ))
+}
+
+async fn contact_edit(State(state): State<Arc<RwLock<PersonInfo>>>) -> impl IntoResponse {
+    let person = state.read().await;
+    Html(format!(
+        r#"<form hx-put="/contact/1" hx-target="this" hx-swap="outerHTML">
+          <div>
+            <label>First Name</label>
+            <input type="text" name="first_name" value="{}">
+          </div>
+          <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" name="last_name" value="{}">
+          </div>
+          <div class="form-group">
+            <label>Email Address</label>
+            <input type="email" name="email" value="{}">
+          </div>
+          <button class="btn">Submit</button>
+          <button class="btn" hx-get="/contact/1">Cancel</button>
+        </form>"#,
+        person.first_name, person.last_name, person.email,
+    ))
+}
+
+// this should save the form submission
+// and turn the editable form back to view-only
+async fn contact_put(
+    State(state): State<Arc<RwLock<PersonInfo>>>,
+    Form(new_person): Form<PersonInfo>,
+) -> impl IntoResponse {
+    {
+        let mut person = state.write().await;
+        *person = new_person.clone();
+    }
+    Html(view_only_person(&new_person))
+}
+
+async fn cancel_edit(State(state): State<Arc<RwLock<PersonInfo>>>) -> impl IntoResponse {
+    let person = state.read().await;
+    Html(view_only_person(&person))
+}
---
+++ HtmxRustZig/htmx_httpz/src/main.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+const httpz = @import("httpz");
+
+const PORT = 8801;
+
+const Person = struct { first_name: []u8, last_name: []u8, email: []u8 };
+
+var stored_person: Person = undefined;
+var stored_person_lock: std.Thread.RwLock = .{};
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
+
+pub fn main() !void {
+    const john: []u8 = try allocator.alloc(u8, "John".len);
+    @memcpy(john, "John");
+
+    const smith: []u8 = try allocator.alloc(u8, "Smith".len);
+    @memcpy(smith, "Smith");
+
+    const email: []u8 = try allocator.alloc(u8, "jon@companyco.com".len);
+    @memcpy(email, "jon@companyco.com");
+
+    stored_person = Person{ .first_name = john, .last_name = smith, .email = email };
+
+    var server = try httpz.Server(void).init(allocator, .{
+        .port = PORT,
+        .request = .{
+            .max_form_count = 20,
+        },
+    }, {});
+
+    defer server.deinit();
+    defer server.stop();
+    var router = server.router(.{});
+
+    router.get("/", index, .{});
+    router.get("/contact/1/edit", contact_edit, .{});
+    router.put("/contact/1", contact_put, .{});
+    router.get("/contact/1/", cancel_edit, .{});
+
+    std.debug.print("listening http://localhost:{d}/\n", .{PORT});
+
+    try server.listen();
+}
+
+fn view_only_person(person: *Person) ![]u8 {
+    return try std.fmt.allocPrint(allocator,
+        \\       <div hx-target="this" hx-swap="outerHTML">
+        \\           <div><label>First Name</label>: {s}</div>
+        \\           <div><label>Last Name</label>: {s}</div>
+        \\           <div><label>Email</label>: {s}</div>
+        \\           <button hx-get="/contact/1/edit" class="btn primary">
+        \\           Click To Edit
+        \\           </button>
+        \\       </div>
+    , .{ person.first_name, person.last_name, person.email });
+}
+
+fn index(_: *httpz.Request, res: *httpz.Response) !void {
+    stored_person_lock.lockShared();
+    defer stored_person_lock.unlockShared();
+    res.body = try std.fmt.allocPrint(allocator,
+        \\           <!DOCTYPE html>
+        \\           <html>
+        \\           <head>
+        \\               <title>+ HTMX</title>
+        \\               <script src="https://unpkg.com/htmx.org"></script>
+        \\               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+        \\           </head>
+        \\           <body>
+        \\           {s}
+        \\           </body>
+        \\           </html>
+    , .{try view_only_person(&stored_person)});
+}
+
+fn contact_edit(_: *httpz.Request, res: *httpz.Response) !void {
+    stored_person_lock.lockShared();
+    defer stored_person_lock.unlockShared();
+    res.body = try std.fmt.allocPrint(allocator,
+        \\<form hx-put="/contact/1" hx-target="this" hx-swap="outerHTML">
+        \\<div>
+        \\<label>First Name</label>
+        \\<input type="text" name="first_name" value="{s}">
+        \\</div>
+        \\<div class="form-group">
+        \\<label>Last Name</label>
+        \\<input type="text" name="last_name" value="{s}">
+        \\</div>
+        \\<div class="form-group">
+        \\<label>Email Address</label>
+        \\<input type="email" name="email" value="{s}">
+        \\</div>
+        \\<button class="btn">Submit</button>
+        \\<button class="btn" hx-get="/contact/1">Cancel</button>
+        \\</form>
+    , .{ stored_person.first_name, stored_person.last_name, stored_person.email });
+}
+
+fn cancel_edit(_: *httpz.Request, res: *httpz.Response) !void {
+    stored_person_lock.lockShared();
+    defer stored_person_lock.unlockShared();
+    res.body = try view_only_person(&stored_person);
+}
+
+fn contact_put(req: *httpz.Request, res: *httpz.Response) !void {
+    stored_person_lock.lock();
+    defer stored_person_lock.unlock();
+    var it = (try req.formData()).iterator();
+
+    res.content_type = .TEXT;
+
+    while (it.next()) |kv| {
+        if (std.mem.eql(u8, kv.key, "first_name")) {
+            stored_person.first_name = try allocator.realloc(stored_person.first_name, kv.value.len);
+            @memcpy(stored_person.first_name, kv.value);
+        } else if (std.mem.eql(u8, kv.key, "last_name")) {
+            stored_person.last_name = try allocator.realloc(stored_person.last_name, kv.value.len);
+            @memcpy(stored_person.last_name, kv.value);
+        } else if (std.mem.eql(u8, kv.key, "email")) {
+            stored_person.email = try allocator.realloc(stored_person.email, kv.value.len);
+            @memcpy(stored_person.email, kv.value);
+        }
+    }
+
+    res.body = try view_only_person(&stored_person);
+}
