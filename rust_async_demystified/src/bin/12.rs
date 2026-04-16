fd5ecef30dc818637f5096f8b61befa4d0061651
Ken 🏈 <me163@mac.com>
2026-04-16 08:42:33 -0700

    indentation thing

--- rust_async_demystified/src/bin/12.rs
+++ rust_async_demystified/src/bin/12.rs
@@ -3,9 +3,9 @@ use futures::future::join_all;
 #[tokio::main(worker_threads = 2)]
 pub async fn main() {
     let handles = (0..=500).map(|_|  {
-            tokio::task::spawn_blocking(|| {
-                get_page("https://news.ycombinator.com")
-            })
+        tokio::task::spawn_blocking(|| {
+            get_page("https://news.ycombinator.com")
+        })
     });
 
     join_all(handles).await;
